6510894df954cbbe4d311f4701c4f861ca4c9343
Jie Lin <jl987@cornell.edu>
2015-05-03 13:20:41 -0400

    Added BioLemmatization.

---
+++ src/test/reciter/lucene/BioLemmatization.java
@@ -0,0 +1,12 @@
+package test.reciter.lucene;
+
+import edu.ucdenver.ccp.nlp.biolemmatizer.BioLemmatizer;
+
+public class BioLemmatization {
+
+	public static void main(String[] args) {
+		
+		String[] a = new String[]{"Transcription"};
+		BioLemmatizer.main(a);
+	}
+}
