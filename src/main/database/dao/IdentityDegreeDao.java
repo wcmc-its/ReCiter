c7c097c8ba9480940647629946c3a0a15cb4ebc3
Jie Lin <jl987@cornell.edu>
2015-05-05 21:05:22 -0400

    Adding IdentityDegree and IdentityDegreeDao.

---
+++ src/main/database/dao/IdentityDegreeDao.java
@@ -0,0 +1,38 @@
+package main.database.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import main.database.DbConnectionFactory;
+import main.database.DbUtil;
+
+public class IdentityDegreeDao {
+	
+	public IdentityDegree getIdentityDegreeByCwid(String cwid) {
+		
+		Connection con = DbConnectionFactory.getConnection();
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		String query = "SELECT bachelor, masters, doctoral WHERE cwid='" + cwid + "'";
+		IdentityDegree identityDegree = new IdentityDegree();
+		identityDegree.setCwid(cwid);
+		try {
+			pst = con.prepareStatement(query);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				identityDegree.setBachelor(rs.getInt(1));
+				identityDegree.setMasters(rs.getInt(2));
+				identityDegree.setDoctoral(rs.getInt(3));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DbUtil.close(rs);
+			DbUtil.close(pst);
+			DbUtil.close(con);
+		}
+		return identityDegree;
+	}
+}
