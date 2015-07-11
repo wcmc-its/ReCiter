11827aa1c1568a07bd206b3f00c9fd168d17656c
Jie Lin <jl987@cornell.edu>
2015-07-10 23:38:48 -0400

    Adding coauthor affiliation and its DAO class.

---
+++ src/main/java/database/model/CoauthorAffiliations.java
@@ -0,0 +1,28 @@
+package database.model;
+
+public class CoauthorAffiliations {
+
+	private int affiliationId;
+	private String label;
+	private double score;
+	public int getAffiliationId() {
+		return affiliationId;
+	}
+	public void setAffiliationId(int affiliationId) {
+		this.affiliationId = affiliationId;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	
+}
