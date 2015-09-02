d5b46749034721df5c722ee2b9051cc5c82eb5b2
Jie Lin <jl987@cornell.edu>
2015-09-01 23:02:06 -0400

    Adding analysis DAO and implementation.

---
+++ src/main/java/database/dao/AnalysisDao.java
@@ -0,0 +1,10 @@
+package database.dao;
+
+import java.util.List;
+
+import database.model.Analysis;
+
+public interface AnalysisDao {
+
+	void insertAnalysisList(List<Analysis> analysisList);
+}
---
+++ src/main/java/database/dao/impl/AnalysisDaoImpl.java
@@ -0,0 +1,16 @@
+package database.dao.impl;
+
+import java.util.List;
+
+import database.dao.AnalysisDao;
+import database.model.Analysis;
+
+public class AnalysisDaoImpl implements AnalysisDao {
+
+	@Override
+	public void insertAnalysisList(List<Analysis> analysisList) {
+
+		
+	}
+
+}
