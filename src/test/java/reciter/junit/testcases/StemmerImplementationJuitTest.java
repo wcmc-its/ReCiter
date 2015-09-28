d12c321abf2891b4df305ec4f9c39992216006da
nnaraparaju <nnaraparaju@gemini-us.com>
2015-09-28 20:56:29 +0530

    Test Case for Issue #17

--- src/test/java/reciter/junit/testcases/StemmerImplementationJuitTest.java
+++ src/test/java/reciter/junit/testcases/StemmerImplementationJuitTest.java
@@ -1,8 +1,6 @@
 package reciter.junit.testcases;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
