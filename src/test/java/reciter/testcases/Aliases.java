e91503f9241a2e03dd02e047dceee25f02ccc2d3
nnaraparaju <nnaraparaju@GCS-LT-023.GCS>
2015-10-31 18:38:34 +0530

    Issue #100 Updated Test Case

--- src/test/java/reciter/testcases/Aliases.java
+++ src/test/java/reciter/testcases/Aliases.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import reciter.algorithm.cluster.ReCiterExample;
 import reciter.model.article.ReCiterArticle;
 import xmlparser.pubmed.PubmedXmlFetcher;
 import xmlparser.pubmed.model.PubmedArticle;
@@ -21,23 +18,22 @@ import database.dao.impl.IdentityDaoImpl;
 import database.model.Identity;
 
 public class Aliases {
-	private final static Logger slf4jLogger = LoggerFactory
-			.getLogger(ReCiterExample.class);
-	Identity identity = null;
+
 	PubmedXmlFetcher pubmedXmlFetcher;
 	List<PubmedArticle> pubmedArticleList;
 	List<ReCiterArticle> reCiterArticleList;
 	List<String> gspPmidList;
+	Identity identity = null;
 
 	@Before
 	public void setUp() throws Exception {
+
 		IdentityDaoImpl dao = new IdentityDaoImpl();
 		identity = dao.getIdentityByCwid(TestController.cwid_junit);
 		pubmedXmlFetcher = new PubmedXmlFetcher();
 		pubmedArticleList = pubmedXmlFetcher.getPubmedArticle(
 				identity.getLastName(), identity.getFirstInitial(),
 				identity.getMiddleName(), TestController.cwid_junit);
-
 		ScopusXmlFetcher scopusXmlFetcher = new ScopusXmlFetcher();
 		reCiterArticleList = new ArrayList<ReCiterArticle>();
 		GoldStandardPmidsDao gspDao = new GoldStandardPmidsDaoImpl();
@@ -52,17 +48,12 @@ public class Aliases {
 			reCiterArticleList.add(ArticleTranslator.translate(pubmedArticle,
 					scopusArticle));
 		}
+
 	}
 
 	@Test
 	public void test() {
-		
-		boolean Test = true;
-		if (Test)
-			slf4jLogger
-					.info("Article is encoded to utf 8 characters , Test Passed");
-		else
-			slf4jLogger.info(" Test Failed ");
+		// Added Logs in PubmedXmlFetcher for the specific circumstance
 
 	}
 }
