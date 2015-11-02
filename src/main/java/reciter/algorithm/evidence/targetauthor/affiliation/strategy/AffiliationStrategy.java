e8bc145eda0d52bae38324ba6713c9a21082ff80
htadimeti@gemini-us.com <htadimeti@gemini-us.com>
2015-11-02 19:03:36 +0530

    fix for #104

--- src/main/java/reciter/algorithm/evidence/targetauthor/affiliation/strategy/AffiliationStrategy.java
+++ src/main/java/reciter/algorithm/evidence/targetauthor/affiliation/strategy/AffiliationStrategy.java
@@ -1,7 +1,15 @@
 package reciter.algorithm.evidence.targetauthor.affiliation.strategy;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import database.dao.IdentityEarliestStartDateDao;
+import database.dao.impl.IdentityEarliestStartDateDaoImpl;
+import database.model.IdentityEarliestStartDate;
 import reciter.algorithm.evidence.targetauthor.AbstractTargetAuthorStrategy;
 import reciter.model.article.ReCiterArticle;
 import reciter.model.author.TargetAuthor;
@@ -12,7 +20,21 @@ public class AffiliationStrategy extends AbstractTargetAuthorStrategy {
 	public double executeStrategy(ReCiterArticle reCiterArticle, TargetAuthor targetAuthor) {
 		boolean containsAffiliation = containsWeillCornell(reCiterArticle);
 		if (containsAffiliation) {
-			return 1;
+			//  Decrease likelihood of institution match if paper was published before target author's start date #104
+			IdentityEarliestStartDateDao dao = new IdentityEarliestStartDateDaoImpl() ;
+			IdentityEarliestStartDate date = dao.getIdentityEarliestStartDateByCwid(targetAuthor.getCwid()); 
+			if(date!=null){
+				DateFormat format = new SimpleDateFormat("dd-MMM-yy", Locale.ENGLISH);
+				Date startDate;
+				try {
+					startDate = format.parse(date.getStartDate());
+					if(reCiterArticle.getJournal().getJournalIssuePubDateYear()>=startDate.getYear())return 1;
+					else return 0;
+				} catch (ParseException e) {					
+					e.printStackTrace();
+					return 1;
+				}				
+			}else return 1;
 		} else {
 			return 0;
 		}
