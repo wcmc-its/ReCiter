292a1b7e103d54213792010f040af2b134a9e6df
Jie <jl987@cornell.edu>
2016-06-25 12:18:18 -0400

    Successfully inserted ESearch results for a given cwid into Mongo.

--- src/main/java/reciter/database/mongo/repository/impl/ESearchResultRepositoryImpl.java
+++ src/main/java/reciter/database/mongo/repository/impl/ESearchResultRepositoryImpl.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.WriteResult;
 
 import reciter.database.mongo.model.ESearchResult;
@@ -22,10 +20,9 @@ public class ESearchResultRepositoryImpl implements ESearchResultRepositoryCusto
 	public boolean pushESearchResult(ESearchResult eSearchResult) {
 		String cwid = eSearchResult.getCwid();
 		List<Long> pmids = eSearchResult.getPmids();
-		WriteResult writeResult = mongoTemplate.updateFirst(
-	            Query.query(Criteria.where("cwid").is(cwid)), 
-	            new Update().addToSet("pmids", pmids.toArray()), ESearchResult.class);
-		
+		BasicDBObject basicDbObject = new BasicDBObject("$addToSet", new BasicDBObject("pmids", new BasicDBObject("$each", pmids)));
+		WriteResult writeResult = mongoTemplate.getCollection("esearchresult").update(new BasicDBObject("cwid", cwid), basicDbObject, true, false);
+		System.out.println("writeResult=[" + writeResult.wasAcknowledged() + "].");
 		return writeResult.wasAcknowledged();
 	}
 }
