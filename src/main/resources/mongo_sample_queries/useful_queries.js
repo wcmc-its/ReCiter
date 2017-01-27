f4d2ca1bce8ca0f36e4e6a31950902dc44e88024
Michael Bales <michaelbales1@gmail.com>
2017-01-27 13:52:31 -0500

    Update useful_queries.js
    Added outline and re-ordered queries to match order in https://www.gitbook.com/book/jl987-jie/reciter-wiki/edit#/edit/master/query-the-mongodb-database.md

--- src/main/resources/mongo_sample_queries/useful_queries.js
+++ src/main/resources/mongo_sample_queries/useful_queries.js
@@ -1,51 +1,11 @@
 use reciter;
 
-/* find average precision and recall in analysis */
-db.analysis.aggregate([
-{
-	$group: {
-		_id:null, precision: {$avg:"$analysis.precision"}, recall: {$avg:"$analysis.recall"}
-	}
-}
-]);
-
-db.meshterm.find();
-/* List all uids */
-db.analysis.find({"_id": "rbaergen"});
-
-db.analysis.count();
-
-/* Find all uids whose precision is less than 0.1 */
-db.analysis.find({"analysis.precision": {"$lt" : 0.1}});
-
-/* Find all uids whose recall is less than 0.1 */
-db.analysis.find({"analysis.recall": {"$lt" : 0.1}});
-
-/* Find all gold standard */
-db.goldstandard.find();
-
-/* Find all gold standard for a given uid */
-db.goldstandard.find({"_id" : "rak2007"});
-
-/* Add pmid 26976629 to gold standard for 'cnathan' */
-db.goldstandard.update({"_id": "mlg2007"}, {$addToSet: {"knownPmids": NumberLong(26898884)}});
-
-/* Remove from gold standard for 'cnathan' an erroneous array [26976629] */
-db.goldstandard.update({"_id": "cnathan"}, {$pull: {"knownPmids": [26976629]}});
-
-/* Remove from gold standard for 'cnathan' an erroneous pmid NumberLong(26976629) */
-db.goldstandard.update({"_id": "cnathan"}, {$pull: {"knownPmids": NumberLong(26976629)}});
-
-/* Find if PMID exist in this uid */
-db.goldstandard.find({"_id": "rgcryst", "knownPmids": {$in: [NumberLong(26927796)]}});
+/* Viewing data */
 
 /* Total number of documents in this identity collection */
 db.identity.count();
 
-/* Find all identities */
-db.identity.find();
-
-/* Find identity whose uid is 'rgcryst' */
+/* View all available data for a given UID */
 db.identity.find({"_id": "rgcryst"});
 
 /* Find pubmed article with PMID 18341570 */
@@ -54,8 +14,20 @@ db.pubmedarticle.find({"_id": 18341570});
 /* Find the PubMed search strategies for uid 'amb9023' */
 db.esearchresult.find({"uid": "wcb2001"});
 
-/* Delete the esearch result where the uid is 'rbaergen' */
-db.esearchresult.deleteMany({"uid": "rbaergen"});
+/* Find all identities */
+db.identity.find();
+
+/* Find identity whose uid is 'rgcryst' */
+db.identity.find({"_id": "rgcryst"});
+
+/* Working with the gold standard */
+
+/* Get a list of all UIDs in the gold standard */
+
+db.goldstandard.find();
+
+/* Get a list of all PMIDs in the gold standard for a given UID */
+db.goldstandard.find({"_id" : "rak2007"});
 
 /** Updating gold standard for rak2007 */
 db.goldstandard.update({"_id": "rgcryst"}, {$addToSet: {"knownPmids": {$each: [
@@ -78,6 +50,47 @@ NumberLong(26026937),
 NumberLong(26116571),
 NumberLong(26728717)
 ]}}});
+
+/* Add pmid 26976629 to gold standard for 'cnathan' */
+db.goldstandard.update({"_id": "mlg2007"}, {$addToSet: {"knownPmids": NumberLong(26898884)}});
+
+/* Remove from gold standard for 'cnathan' an erroneous array [26976629] */
+db.goldstandard.update({"_id": "cnathan"}, {$pull: {"knownPmids": [26976629]}});
+
+/* Remove from gold standard for 'cnathan' an erroneous pmid NumberLong(26976629) */
+db.goldstandard.update({"_id": "cnathan"}, {$pull: {"knownPmids": NumberLong(26976629)}});
+
+/* Find if PMID exist in this uid */
+db.goldstandard.find({"_id": "rgcryst", "knownPmids": {$in: [NumberLong(26927796)]}});
+
+/* Working with analysis results */
+
+/* Find average precision and recall in analysis */
+db.analysis.aggregate([
+{
+	$group: {
+		_id:null, precision: {$avg:"$analysis.precision"}, recall: {$avg:"$analysis.recall"}
+	}
+}
+]);
+
+/* Display the results of the analysis for a given UID */
+db.analysis.find({"_id": "rbaergen"});
+
+/* Find all uids whose precision is less than 0.1 */
+db.analysis.find({"analysis.precision": {"$lt" : 0.1}});
+
+/* Find all uids whose recall is less than 0.1 */
+db.analysis.find({"analysis.recall": {"$lt" : 0.1}});
+
+/* Additional query examples */
+
+/* Delete the esearch result where the uid is 'rbaergen' */
+db.esearchresult.deleteMany({"uid": "rbaergen"});
+
+db.meshterm.find();
+
+db.analysis.count();
 
 db.recitercluster.find();
 
