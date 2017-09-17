71f80ac0c4243d5ec1993c9003c4aa2d99ccd5d3
jl987-Jie <jl987@cornell.edu>
2017-09-16 20:01:48 -0700

    Added @EnableScan annotation

--- src/main/java/reciter/database/dynamodb/repository/PubMedArticleRepository.java
+++ src/main/java/reciter/database/dynamodb/repository/PubMedArticleRepository.java
@@ -1,7 +1,9 @@
 package reciter.database.dynamodb.repository;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import reciter.database.dynamodb.model.PubMedArticle;
 
+@EnableScan
 public interface PubMedArticleRepository extends CrudRepository<PubMedArticle, Long> {
 }
--- src/main/java/reciter/database/dynamodb/repository/ScopusArticleRepository.java
+++ src/main/java/reciter/database/dynamodb/repository/ScopusArticleRepository.java
@@ -1,7 +1,9 @@
 package reciter.database.dynamodb.repository;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import reciter.database.dynamodb.model.ScopusArticle;
 
+@EnableScan
 public interface ScopusArticleRepository extends CrudRepository<ScopusArticle, Long> {
 }
