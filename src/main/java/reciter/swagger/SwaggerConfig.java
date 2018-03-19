0b82a162294a864b314cb202451fc921e48be3fa
jl987-Jie <jl987@cornell.edu>
2018-03-18 23:14:45 -0700

    Add Swagger Configuration.

---
+++ src/main/java/reciter/swagger/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package reciter.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("reciter.controller"))
+                .paths(regex("/reciter.*"))
+                .build();
+    }
+}
