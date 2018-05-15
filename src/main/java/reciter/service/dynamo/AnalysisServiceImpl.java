9d1d8ffd63d62a0e7c16150e731d8977ba525853
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-05-15 15:53:47 -0400

    Analysis service and implementation for crud operations

---
+++ src/main/java/reciter/service/dynamo/AnalysisServiceImpl.java
@@ -0,0 +1,37 @@
+package reciter.service.dynamo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import reciter.database.dynamodb.model.AnalysisOutput;
+import reciter.database.dynamodb.repository.AnalysisOutputRepository;
+import reciter.service.AnalysisService;
+
+@Service("AnalysisOutputService")
+public class AnalysisServiceImpl implements AnalysisService{
+	
+	 @Autowired
+	private AnalysisOutputRepository analysisOutputRepository;
+
+	@Override
+	public void save(AnalysisOutput analysis) {
+		analysisOutputRepository.save(analysis);
+	}
+
+	@Override
+	public AnalysisOutput findByUid(String uid) {
+		return analysisOutputRepository.findOne(uid);
+	}
+
+	@Override
+	public void deleteAll() {
+		 analysisOutputRepository.deleteAll();
+	}
+
+	@Override
+	public void delete(String uid) {
+		analysisOutputRepository.delete(uid);
+	}
+	
+
+}
