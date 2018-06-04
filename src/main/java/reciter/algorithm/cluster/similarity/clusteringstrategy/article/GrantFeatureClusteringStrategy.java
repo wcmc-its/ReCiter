e8b13c1640cd140b89df5abef59d446de4e04bce
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-06-04 16:52:09 -0400

    Grant identifier strategy

--- src/main/java/reciter/algorithm/cluster/similarity/clusteringstrategy/article/GrantFeatureClusteringStrategy.java
+++ src/main/java/reciter/algorithm/cluster/similarity/clusteringstrategy/article/GrantFeatureClusteringStrategy.java
@@ -1,5 +1,31 @@
 package reciter.algorithm.cluster.similarity.clusteringstrategy.article;
 
-public class GrantFeatureClusteringStrategy {
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import reciter.algorithm.cluster.clusteringstrategy.article.AbstractClusteringStrategy;
+import reciter.algorithm.cluster.model.ReCiterCluster;
+import reciter.model.article.ReCiterArticle;
+
+public class GrantFeatureClusteringStrategy extends AbstractClusteringStrategy {
+
+	@Override
+	public Map<Long, ReCiterCluster> cluster(List<ReCiterArticle> reCiterArticles) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Long, ReCiterCluster> cluster(List<ReCiterArticle> reCiterArticles, Set<Long> seedPmids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Long, ReCiterCluster> cluster(Map<Long, ReCiterCluster> clusters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
