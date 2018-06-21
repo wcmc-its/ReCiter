c37c6c8213074bf64397dea4a31c48c344cbfa04
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-06-21 16:38:56 -0400

    ScoreByNameStrategy implementation

---
+++ src/main/java/reciter/algorithm/evidence/targetauthor/name/ScoreByNameStrategyContext.java
@@ -0,0 +1,12 @@
+package reciter.algorithm.evidence.targetauthor.name;
+
+import reciter.algorithm.evidence.targetauthor.AbstractTargetAuthorStrategyContext;
+import reciter.algorithm.evidence.targetauthor.TargetAuthorStrategy;
+
+public class ScoreByNameStrategyContext extends AbstractTargetAuthorStrategyContext {
+
+	public ScoreByNameStrategyContext(TargetAuthorStrategy strategy) {
+		super(strategy);
+	}
+
+}
