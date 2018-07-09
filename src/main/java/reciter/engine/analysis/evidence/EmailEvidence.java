e8709ddbbb324daf83bb5fc16cc2094ecd606eea
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-07-09 16:07:11 -0400

    email evidence model

---
+++ src/main/java/reciter/engine/analysis/evidence/EmailEvidence.java
@@ -0,0 +1,15 @@
+package reciter.engine.analysis.evidence;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@DynamoDBDocument
+public class EmailEvidence {
+	
+	private String emailMatch;
+	private double emailMatchScore;
+}
