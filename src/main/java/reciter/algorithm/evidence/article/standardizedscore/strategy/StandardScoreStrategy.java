becd915ae1a26b18c99b301e848194412a238713
Sarbajit Dutta <szd2013@med.cornell.edu>
2018-07-20 16:22:07 -0400

    round of all the scores to upto 2 decimal places

--- src/main/java/reciter/algorithm/evidence/article/standardizedscore/strategy/StandardScoreStrategy.java
+++ src/main/java/reciter/algorithm/evidence/article/standardizedscore/strategy/StandardScoreStrategy.java
@@ -1,10 +1,12 @@
 package reciter.algorithm.evidence.article.standardizedscore.strategy;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import reciter.algorithm.cluster.article.scorer.ReCiterArticleScorer;
 import reciter.algorithm.evidence.article.AbstractReCiterArticleStrategy;
 import reciter.algorithm.evidence.article.acceptedrejected.strategy.AcceptedRejectedStrategy;
 import reciter.model.article.ReCiterArticle;
@@ -12,6 +14,13 @@ import reciter.model.article.ReCiterArticle;
 public class StandardScoreStrategy extends AbstractReCiterArticleStrategy {
 	
 	private static final Logger slf4jLogger = LoggerFactory.getLogger(AcceptedRejectedStrategy.class);
+	
+	private final String[] standardizedScoreMapping = ReCiterArticleScorer.strategyParameters.getStandardizedScoreMapping().trim().split(",");
+	private List<String> standardizedScores;
+	
+	public StandardScoreStrategy() {
+		this.standardizedScores = Arrays.asList(standardizedScoreMapping);
+	}
 
 	@Override
 	public double executeStrategy(ReCiterArticle reCiterArticle, ReCiterArticle otherReCiterArticle) {
@@ -28,6 +37,22 @@ public class StandardScoreStrategy extends AbstractReCiterArticleStrategy {
 	@Override
 	public double executeStrategy(List<ReCiterArticle> reCiterArticles) {
 		reCiterArticles.stream().forEach(reCiterArticle -> {
+			double standardizedScore = 1;
+			double totalArticleScoreNonStandardizedExcludingFeedbackScore = reCiterArticle.getTotalArticleScoreNonStandardized() -
+					((reCiterArticle.getAcceptedRejectedEvidence().getFeedbackScoreAccepted() !=null)?reCiterArticle.getAcceptedRejectedEvidence().getFeedbackScoreAccepted():0) - 
+					((reCiterArticle.getAcceptedRejectedEvidence().getFeedbackScoreRejected() !=null)?reCiterArticle.getAcceptedRejectedEvidence().getFeedbackScoreRejected():0);
+			for(int i = 0; i < this.standardizedScores.size(); i++) {
+					if(i == this.standardizedScores.size() - 1) {
+						if(totalArticleScoreNonStandardizedExcludingFeedbackScore >= Double.parseDouble(this.standardizedScores.get(i))) {
+							reCiterArticle.setTotalArticleScoreStandardized(standardizedScore);
+						}
+					} else if(totalArticleScoreNonStandardizedExcludingFeedbackScore >= Double.parseDouble(this.standardizedScores.get(i)) 
+							&&
+							totalArticleScoreNonStandardizedExcludingFeedbackScore < Double.parseDouble(this.standardizedScores.get(i + 1))) {
+						reCiterArticle.setTotalArticleScoreStandardized(standardizedScore);
+					}
+					standardizedScore++;
+			}
 			
 		});
 		return 0;
