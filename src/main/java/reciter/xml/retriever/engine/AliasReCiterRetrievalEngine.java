cade9d46e0fd293c2cc577ecb02777fa91efe623
Sarbajit Dutta <szd2013@med.cornell.edu>
2018-08-09 19:01:29 -0400

    refactor of entire strategy and use of date range

--- src/main/java/reciter/xml/retriever/engine/AliasReCiterRetrievalEngine.java
+++ src/main/java/reciter/xml/retriever/engine/AliasReCiterRetrievalEngine.java
@@ -30,6 +30,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import reciter.algorithm.util.ReCiterStringUtil;
 import reciter.model.identity.AuthorName;
 import reciter.model.identity.Identity;
 import reciter.model.identity.PubMedAlias;
@@ -52,6 +53,11 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 	
 	@Autowired
 	private ESearchResultService eSearchResultService;
+	
+	public enum IdentityNameType {
+		ORIGINAL,
+		DERIVED
+	}
 	
 	private class AsyncRetrievalEngine extends Thread {
 
@@ -118,14 +124,18 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 		
 		String uid = identity.getUid();
 		
+		Map<IdentityNameType, Set<AuthorName>> identityNames = new HashMap<IdentityNameType, Set<AuthorName>>();
+		identityAuthorNames(identity, identityNames);
+		boolean useStrictQueryOnly = identityNames.entrySet().stream().anyMatch(entry -> entry.getKey() == IdentityNameType.DERIVED && entry.getValue().size() > 0);
+		
 		//Retreive by GoldStandard
-		RetrievalResult goldStandardRetrievalResult = goldStandardRetrievalStrategy.retrievePubMedArticles(identity);
+		RetrievalResult goldStandardRetrievalResult = goldStandardRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
 		Map<Long, PubMedArticle> pubMedArticles = goldStandardRetrievalResult.getPubMedArticles();
 		savePubMedArticles(pubMedArticles.values(), uid, goldStandardRetrievalStrategy.getRetrievalStrategyName(), goldStandardRetrievalResult.getPubMedQueryResults());
 		
 		
 		// Retrieve by email.
-		RetrievalResult retrievalResult = emailRetrievalStrategy.retrievePubMedArticles(identity);
+		RetrievalResult retrievalResult = emailRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
 		pubMedArticles = retrievalResult.getPubMedArticles();
 		
 		if (pubMedArticles.size() > 0) {
@@ -154,8 +164,12 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 		
 		// TODO parallelize by putting save in a separate thread.
 		savePubMedArticles(pubMedArticles.values(), uid, emailRetrievalStrategy.getRetrievalStrategyName(), retrievalResult.getPubMedQueryResults());
-		
-		RetrievalResult r1 = firstNameInitialRetrievalStrategy.retrievePubMedArticles(identity);
+		RetrievalResult r1;
+		if(useStrictQueryOnly) {
+			r1 = firstNameInitialRetrievalStrategy.retrievePubMedArticles(identity, identityNames, false);
+		} else {
+			r1 = firstNameInitialRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
+		}
 		//if (r1.getPubMedArticles().size() > 0) {
 		if(r1.getPubMedQueryResults() != null
 				&&
@@ -167,23 +181,33 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 			pubMedArticles.putAll(r1.getPubMedArticles());
 			savePubMedArticles(r1.getPubMedArticles().values(), uid, firstNameInitialRetrievalStrategy.getRetrievalStrategyName(), r1.getPubMedQueryResults());
 			uniquePmids.addAll(r1.getPubMedArticles().keySet());
-		} else {
-			RetrievalResult r2 = affiliationInDbRetrievalStrategy.retrievePubMedArticles(identity);
+		} 
+		
+		if(r1.getPubMedQueryResults() != null
+				&&
+				r1.getPubMedQueryResults().size() > 0
+				&&
+				r1.getPubMedQueryResults().get(0).isUsed()
+				&&
+				r1.getPubMedQueryResults().get(0).getNumResult() > searchStrategyLeninentThreshold
+				||
+				useStrictQueryOnly) {
+			RetrievalResult r2 = affiliationInDbRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
 			pubMedArticles.putAll(r2.getPubMedArticles());
 			savePubMedArticles(r2.getPubMedArticles().values(), uid, affiliationInDbRetrievalStrategy.getRetrievalStrategyName(), r2.getPubMedQueryResults());
 			uniquePmids.addAll(r2.getPubMedArticles().keySet());
 			
-			RetrievalResult r3 = affiliationRetrievalStrategy.retrievePubMedArticles(identity);
+			RetrievalResult r3 = affiliationRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
 			pubMedArticles.putAll(r3.getPubMedArticles());
 			savePubMedArticles(r3.getPubMedArticles().values(), uid, affiliationRetrievalStrategy.getRetrievalStrategyName(), r3.getPubMedQueryResults());
 			uniquePmids.addAll(r3.getPubMedArticles().keySet());
 			
-			RetrievalResult r4 = departmentRetrievalStrategy.retrievePubMedArticles(identity);
+			RetrievalResult r4 = departmentRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
 			pubMedArticles.putAll(r4.getPubMedArticles());
 			savePubMedArticles(r4.getPubMedArticles().values(), uid, departmentRetrievalStrategy.getRetrievalStrategyName(), r4.getPubMedQueryResults());
 			uniquePmids.addAll(r4.getPubMedArticles().keySet());
 			
-			RetrievalResult r5 = grantRetrievalStrategy.retrievePubMedArticles(identity);
+			RetrievalResult r5 = grantRetrievalStrategy.retrievePubMedArticles(identity, identityNames, useStrictQueryOnly);
 			pubMedArticles.putAll(r5.getPubMedArticles());
 			savePubMedArticles(r5.getPubMedArticles().values(), uid, grantRetrievalStrategy.getRetrievalStrategyName(), r5.getPubMedQueryResults());
 			uniquePmids.addAll(r5.getPubMedArticles().keySet());
@@ -252,12 +276,17 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 		
 		String uid = identity.getUid();
 		
+		Map<IdentityNameType, Set<AuthorName>> identityNames = new HashMap<IdentityNameType, Set<AuthorName>>();
+		identityAuthorNames(identity, identityNames);
+		
+		boolean useStrictQueryOnly = identityNames.entrySet().stream().anyMatch(entry -> entry.getKey() == IdentityNameType.DERIVED && entry.getValue().size() > 0);
+		
 		//Retreive by GoldStandard
-		RetrievalResult goldStandardRetrievalResult = goldStandardRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+		RetrievalResult goldStandardRetrievalResult = goldStandardRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 		Map<Long, PubMedArticle> pubMedArticles = goldStandardRetrievalResult.getPubMedArticles();
 		
 		// Retrieve by email.
-		RetrievalResult retrievalResult = emailRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+		RetrievalResult retrievalResult = emailRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 		Map<Long, PubMedArticle> emailPubMedArticles = retrievalResult.getPubMedArticles();
 		pubMedArticles = retrievalResult.getPubMedArticles();
 		
@@ -289,7 +318,12 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 		// TODO parallelize by putting save in a separate thread.
 		savePubMedArticles(pubMedArticles.values(), uid, emailRetrievalStrategy.getRetrievalStrategyName(), retrievalResult.getPubMedQueryResults());
 		
-		RetrievalResult r1 = firstNameInitialRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+		RetrievalResult r1;
+		if(useStrictQueryOnly) {
+			r1 = firstNameInitialRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, false);
+		} else {
+			r1 = firstNameInitialRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
+		}
 		//if (r1.getPubMedArticles().size() > 0) {
 		if(r1.getPubMedQueryResults() != null
 				&&
@@ -301,28 +335,38 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 			pubMedArticles.putAll(r1.getPubMedArticles());
 			savePubMedArticles(r1.getPubMedArticles().values(), uid, firstNameInitialRetrievalStrategy.getRetrievalStrategyName(), r1.getPubMedQueryResults());
 			uniquePmids.addAll(r1.getPubMedArticles().keySet());
-		} else {
-			RetrievalResult r2 = affiliationInDbRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+		} 
+		
+		if(r1.getPubMedQueryResults() != null
+				&&
+				r1.getPubMedQueryResults().size() > 0
+				&&
+				r1.getPubMedQueryResults().get(0).isUsed()
+				&&
+				r1.getPubMedQueryResults().get(0).getNumResult() > searchStrategyLeninentThreshold
+				||
+				useStrictQueryOnly) {
+			RetrievalResult r2 = affiliationInDbRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 			pubMedArticles.putAll(r2.getPubMedArticles());
 			savePubMedArticles(r2.getPubMedArticles().values(), uid, affiliationInDbRetrievalStrategy.getRetrievalStrategyName(), r2.getPubMedQueryResults());
 			uniquePmids.addAll(r2.getPubMedArticles().keySet());
 			
-			RetrievalResult r3 = affiliationRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+			RetrievalResult r3 = affiliationRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 			pubMedArticles.putAll(r3.getPubMedArticles());
 			savePubMedArticles(r3.getPubMedArticles().values(), uid, affiliationRetrievalStrategy.getRetrievalStrategyName(), r3.getPubMedQueryResults());
 			uniquePmids.addAll(r3.getPubMedArticles().keySet());
 			
-			RetrievalResult r4 = departmentRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+			RetrievalResult r4 = departmentRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 			pubMedArticles.putAll(r4.getPubMedArticles());
 			savePubMedArticles(r4.getPubMedArticles().values(), uid, departmentRetrievalStrategy.getRetrievalStrategyName(), r4.getPubMedQueryResults());
 			uniquePmids.addAll(r4.getPubMedArticles().keySet());
 			
-			RetrievalResult r5 = grantRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+			RetrievalResult r5 = grantRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 			pubMedArticles.putAll(r5.getPubMedArticles());
 			savePubMedArticles(r5.getPubMedArticles().values(), uid, grantRetrievalStrategy.getRetrievalStrategyName(), r5.getPubMedQueryResults());
 			uniquePmids.addAll(r5.getPubMedArticles().keySet());
 			
-			RetrievalResult r6 = fullNameRetrievalStrategy.retrievePubMedArticles(identity, startDate, endDate);
+			RetrievalResult r6 = fullNameRetrievalStrategy.retrievePubMedArticles(identity, identityNames, startDate, endDate, useStrictQueryOnly);
 			pubMedArticles.putAll(r6.getPubMedArticles());
 			savePubMedArticles(r6.getPubMedArticles().values(), uid, fullNameRetrievalStrategy.getRetrievalStrategyName(), r6.getPubMedQueryResults());
 			uniquePmids.addAll(r6.getPubMedArticles().keySet());
@@ -398,5 +442,70 @@ public class AliasReCiterRetrievalEngine extends AbstractReCiterRetrievalEngine 
 			List<ScopusArticle> scopusArticles = goldStandardRetrievalStrategy.retrieveScopus(pmids);
 			scopusService.save(scopusArticles);
 		}
+	}
+	
+	/**
+	 * This function get all authorNames and derive additional names as well.
+	 * @see <a href ="https://github.com/wcmc-its/ReCiter/issues/259">All Identity Name Sec 3.</a>
+	 * @param identity
+	 * @return
+	 */
+	private void identityAuthorNames(Identity identity, Map<IdentityNameType, Set<AuthorName>> identityNames) {
+		Set<AuthorName> identityAuthorNames  = new HashSet<AuthorName>();
+		Set<AuthorName> identityDerivedNames = new HashSet<AuthorName>();
+		AuthorName identityPrimaryName = identity.getPrimaryName();
+		identityPrimaryName.setFirstName(ReCiterStringUtil.deAccent(identityPrimaryName.getFirstName()));
+		identityPrimaryName.setLastName(ReCiterStringUtil.deAccent(identityPrimaryName.getLastName().replaceAll("(,Jr|, Jr|, MD PhD|,MD PhD|, MD-PhD|,MD-PhD|, PhD|,PhD|, MD|,MD|, III|,III|, II|,II|, Sr|,Sr|Jr|MD PhD|MD-PhD|PhD|MD|III|II|Sr)$", "")));
+		if(identityPrimaryName.getMiddleName() != null) {
+			identityPrimaryName.setMiddleName(ReCiterStringUtil.deAccent(identityPrimaryName.getMiddleName()));
+		}
+		
+		//For any name in primaryName or alternateNames, does targetAuthor have a surname, which satisfies these conditions: 
+		//contains a space or dash; if you break up the name at the first space or dash, there would be two strings of four characters or greater
+		if(identityPrimaryName.getLastName().contains(" ") || identityPrimaryName.getLastName().contains("-")) {
+			identityDerivedNames.addAll(deriveAdditionalName(identityPrimaryName));
+		}
+		
+		identityAuthorNames.add(identityPrimaryName);
+		
+		for(AuthorName authorName: identity.getAlternateNames()) {
+			authorName.setFirstName(ReCiterStringUtil.deAccent(authorName.getFirstName()));
+			authorName.setLastName(ReCiterStringUtil.deAccent(authorName.getLastName().replaceAll("(,Jr|, Jr|, MD PhD|,MD PhD|, MD-PhD|,MD-PhD|, PhD|,PhD|, MD|,MD|, III|,III|, II|,II|, Sr|,Sr|Jr|MD PhD|MD-PhD|PhD|MD|III|II|Sr)$", "")));
+			if(authorName.getMiddleName() != null) {
+				authorName.setMiddleName(ReCiterStringUtil.deAccent(authorName.getMiddleName()));
+			}
+			if(authorName.getLastName().contains(" ") || authorName.getLastName().contains("-")) {
+				identityDerivedNames.addAll(deriveAdditionalName(authorName));
+			}
+			
+			identityAuthorNames.add(identityPrimaryName);
+		}
+		identityNames.put(IdentityNameType.ORIGINAL, identityAuthorNames);
+		identityNames.put(IdentityNameType.DERIVED, identityDerivedNames);
+	}
+	
+	/**
+	 * This function derive additional names, if possible.
+	 * @see <a href ="https://github.com/wcmc-its/ReCiter/issues/259">Additional Name Sec 4.</a>
+	 * @param identityName
+	 * @return
+	 */
+	private Set<AuthorName> deriveAdditionalName(AuthorName identityName) {
+		String[] possibleLastName = identityName.getLastName().split("\\s+|-", 2);
+		if(possibleLastName[0].length() >=4 
+				&&
+				possibleLastName[1].length() >=4) {
+			Set<AuthorName> derivedAuthorNames = new HashSet<AuthorName>();
+			String middleName = null;
+			if(identityName.getMiddleName() != null) {
+				middleName = identityName.getMiddleName();
+			}
+			AuthorName authorName1 = new AuthorName(identityName.getFirstName(), middleName, possibleLastName[0]);
+			AuthorName authorName2 = new AuthorName(identityName.getFirstName(), middleName, possibleLastName[1]);
+			derivedAuthorNames.add(authorName1);
+			derivedAuthorNames.add(authorName2);
+			return derivedAuthorNames;
+		}
+		return null;
 	}
 }
