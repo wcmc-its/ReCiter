d4347854117802a37f23c89c6dabd4d4ca90d218
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-08-14 11:02:04 -0400

    impl of knownRelationshipRetrieval strategy

---
+++ src/main/java/reciter/xml/retriever/pubmed/KnownRelationshipRetrievalStrategy.java
@@ -0,0 +1,82 @@
+package reciter.xml.retriever.pubmed;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import reciter.model.identity.AuthorName;
+import reciter.model.identity.Identity;
+import reciter.model.identity.KnownRelationship;
+import reciter.pubmed.retriever.PubMedQuery;
+import reciter.xml.retriever.pubmed.PubMedQueryType.PubMedQueryBuilder;
+
+/**
+ * @author szd2013
+ * This class gets all the known relationship from Identity such as HR relationship, co-investigator, manager & mentor/mentee and 
+ * creates query to be searched in Pubmed in strict retrieval mode
+ */
+@Component("knownRelationshipRetrievalStrategy")
+public class KnownRelationshipRetrievalStrategy extends AbstractNameRetrievalStrategy {
+	
+	private static final String retrievalStrategyName = "KnownRelationshipRetrievalStrategy";
+	private final static Logger slf4jLogger = LoggerFactory.getLogger(KnownRelationshipRetrievalStrategy.class);
+
+	@Override
+	public String getRetrievalStrategyName() {
+		return retrievalStrategyName;
+	}
+
+	@Override
+	protected PubMedQuery buildNameQuery(Set<AuthorName> identitynames, Identity identity) {
+		PubMedQueryBuilder pubMedQueryBuilder = 
+				new PubMedQueryBuilder(getStrategySpecificKeyword(identity))
+					.author(true, false, identitynames);
+		
+		PubMedQuery query = pubMedQueryBuilder.build();
+		slf4jLogger.info(retrievalStrategyName + " produced query=[" + query + "]");
+		return query;
+	}
+
+	@Override
+	protected PubMedQuery buildNameQuery(Set<AuthorName> identitynames, Identity identity, Date startDate,
+			Date endDate) {
+		PubMedQueryBuilder pubMedQueryBuilder = 
+				new PubMedQueryBuilder(getStrategySpecificKeyword(identity))
+					.author(true, false, identitynames)
+					.dateRange(true, startDate, endDate);
+		
+		return pubMedQueryBuilder.build();
+	}
+
+	@Override
+	protected String getStrategySpecificKeyword(Identity identity) {
+		if (identity.getKnownRelationships() != null && !identity.getKnownRelationships().isEmpty()) {
+			Iterator<KnownRelationship> iter = identity.getKnownRelationships().iterator();
+			final KnownRelationship firstRelationship = iter.next(); 
+			final String first	= firstRelationship.getName().getLastName() + " " + firstRelationship.getName().getFirstInitial() + "[au]";
+			if (!iter.hasNext()) {
+				return first;
+			}
+			
+			//for more than 1 institutions 
+			final StringBuilder buf = new StringBuilder();
+			if(first != null) {
+				buf.append("(" + first);
+			} 
+			while(iter.hasNext()) {
+				KnownRelationship knownRelationship = iter.next();
+				buf.append(" OR ");
+				buf.append(knownRelationship.getName().getLastName() + " " + firstRelationship.getName().getFirstInitial() + "[au]");
+			}
+			buf.append(")");
+			return buf.toString();
+		} else {
+			return null;
+		}
+	}
+
+}
