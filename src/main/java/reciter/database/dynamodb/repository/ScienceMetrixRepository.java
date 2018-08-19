1b26d561c5317e1286401c5a6a2a5f5e9c401ce0
jl987-Jie <jl987@cornell.edu>
2018-08-18 23:22:12 -0700

    Removed unused import in ScienceMetrixRepository.

--- src/main/java/reciter/database/dynamodb/repository/ScienceMetrixRepository.java
+++ src/main/java/reciter/database/dynamodb/repository/ScienceMetrixRepository.java
@@ -4,10 +4,9 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import reciter.database.dynamodb.model.ScienceMetrix;
 
-import java.util.List;
-
 @EnableScan
 public interface ScienceMetrixRepository extends CrudRepository<ScienceMetrix, Long> {
     ScienceMetrix findByEissn(String eissn);
+
     ScienceMetrix findByIssn(String issn);
 }
