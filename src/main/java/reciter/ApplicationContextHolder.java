095c60cb15e3f5eab32def0f5afc2158a36c0b75
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-08-28 16:53:12 -0400

    added applicationContext to be accessed in non spring classes

---
+++ src/main/java/reciter/ApplicationContextHolder.java
@@ -0,0 +1,20 @@
+package reciter;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextHolder implements ApplicationContextAware {
+	private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;   
+    }
+
+    public static ApplicationContext getContext() {
+        return context;
+    }
+}
