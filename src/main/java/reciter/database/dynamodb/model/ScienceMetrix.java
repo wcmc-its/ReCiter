40d3e2e1fc63a022d8d3837ccebbc6e6e932e92d
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-09-04 16:25:31 -0400

    added annotations for csv binding

--- src/main/java/reciter/database/dynamodb/model/ScienceMetrix.java
+++ src/main/java/reciter/database/dynamodb/model/ScienceMetrix.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.opencsv.bean.CsvBindByPosition;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,22 +16,37 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ScienceMetrix {
 	
+	@CsvBindByPosition(position=0)
 	@DynamoDBHashKey(attributeName = "smsid")
     private Long smsid;
+	
+	@CsvBindByPosition(position=3)
 	@DynamoDBIndexHashKey(globalSecondaryIndexName = "eissn-index")
     @DynamoDBAttribute(attributeName = "eissn")
     private String eissn;
+	
+	@CsvBindByPosition(position=2)
 	@DynamoDBIndexHashKey(globalSecondaryIndexName = "issn-index")
     @DynamoDBAttribute(attributeName = "issn")
     private String issn;
+	
+	@CsvBindByPosition(position=1)
 	@DynamoDBAttribute(attributeName = "publicationName")
     private String publicationName;
+	
+	@CsvBindByPosition(position=7)
 	@DynamoDBAttribute(attributeName = "scienceMatrixSubfieldId")
     private String scienceMatrixSubfieldId;
+	
+	@CsvBindByPosition(position=4)
 	@DynamoDBAttribute(attributeName = "scienceMetrixDomain")
     private String scienceMetrixDomain;
+	
+	@CsvBindByPosition(position=5)
 	@DynamoDBAttribute(attributeName = "scienceMetrixField")
     private String scienceMetrixField;
+	
+	@CsvBindByPosition(position=6)
 	@DynamoDBAttribute(attributeName = "scienceMetrixSubfield")
     private String scienceMetrixSubfield;
 
--- src/main/java/reciter/database/dynamodb/model/ScienceMetrixDepartmentCategory.java
+++ src/main/java/reciter/database/dynamodb/model/ScienceMetrixDepartmentCategory.java
@@ -3,6 +3,8 @@ package reciter.database.dynamodb.model;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,15 +16,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ScienceMetrixDepartmentCategory {
 	
+	@CsvBindByPosition(position=0)
 	@DynamoDBHashKey(attributeName = "pk")
-	private Long pk;
+	private int pk;
+	
+	@CsvBindByPosition(position=1)
 	@DynamoDBAttribute(attributeName = "logOddsRatio")
 	private double logOddsRatio;
+	
+	@CsvBindByPosition(position=2)
 	@DynamoDBAttribute(attributeName = "primaryDepartment")
 	private String primaryDepartment;
+	
+	@CsvBindByPosition(position=3)
 	@DynamoDBAttribute(attributeName = "scienceMetrixJournalSubfield")
 	private String scienceMetrixJournalSubfield;
+	
+	@CsvBindByPosition(position=4)
 	@DynamoDBAttribute(attributeName = "scienceMetrixJournalSubfieldId")
-	private Long scienceMetrixJournalSubfieldId;
+	private int scienceMetrixJournalSubfieldId;
 	
 }
