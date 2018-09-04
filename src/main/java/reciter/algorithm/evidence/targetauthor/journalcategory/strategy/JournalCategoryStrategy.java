9c33355277de838efb68041a9114b71b4ffeee66
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-09-04 16:24:23 -0400

    change in datatype

--- src/main/java/reciter/algorithm/evidence/targetauthor/journalcategory/strategy/JournalCategoryStrategy.java
+++ src/main/java/reciter/algorithm/evidence/targetauthor/journalcategory/strategy/JournalCategoryStrategy.java
@@ -84,7 +84,7 @@ public class JournalCategoryStrategy extends AbstractTargetAuthorStrategy {
 						journalCategoryEvidence = new JournalCategoryEvidence();
 						journalCategoryEvidence.setJournalSubfieldScienceMetrixLabel(scienceMetrix.getScienceMetrixSubfield());
 						journalCategoryEvidence.setJournalSubfieldDepartment(scienceMetrix.getScienceMetrixField());
-						journalCategoryEvidence.setJournalSubfieldScienceMetrixID(Long.parseLong(scienceMetrix.getScienceMatrixSubfieldId()));
+						journalCategoryEvidence.setJournalSubfieldScienceMetrixID(Integer.parseInt(scienceMetrix.getScienceMatrixSubfieldId()));
 						journalCategoryEvidence.setJournalSubfieldScore(ReCiterArticleScorer.strategyParameters.getJournalSubfieldScore());
 					}
 				}
