4708add248823599ec436073afac58496153c06a
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-11-07 11:01:08 -0500

    change in relationship type

--- src/main/java/reciter/engine/analysis/evidence/RelationshipEvidence.java
+++ src/main/java/reciter/engine/analysis/evidence/RelationshipEvidence.java
@@ -3,11 +3,14 @@ package reciter.engine.analysis.evidence;
 import java.util.Set;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel.DynamoDBAttributeType;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Data;
 import lombok.ToString;
 import reciter.model.identity.AuthorName;
+import reciter.model.identity.KnownRelationship.RelationshipType;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
