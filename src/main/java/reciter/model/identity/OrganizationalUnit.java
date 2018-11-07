fde6cfe55020a009c9f5ea26cf92125f1c3d84a7
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-11-07 11:01:37 -0500

    added enum type for models

--- src/main/java/reciter/model/identity/OrganizationalUnit.java
+++ src/main/java/reciter/model/identity/OrganizationalUnit.java
@@ -1,18 +1,21 @@
 package reciter.model.identity;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel.DynamoDBAttributeType;
 
 @DynamoDBDocument
 public class OrganizationalUnit {
 	
 	private String organizationalUnitLabel;
-	private String organizationalUnitType;
+	@DynamoDBTyped(DynamoDBAttributeType.S)
+	private OrganizationalUnitType organizationalUnitType;
 	
 	public OrganizationalUnit() {
 		
 	}
 	
-	public OrganizationalUnit(String organizationalUnitLabel, String organizationalUnitType) {
+	public OrganizationalUnit(String organizationalUnitLabel, OrganizationalUnitType organizationalUnitType) {
 		this.organizationalUnitLabel = organizationalUnitLabel;
 		this.organizationalUnitType = organizationalUnitType;
 	}
@@ -23,11 +26,17 @@ public class OrganizationalUnit {
 	public void setOrganizationalUnitLabel(String organizationalUnitLabel) {
 		this.organizationalUnitLabel = organizationalUnitLabel;
 	}
-	public String getOrganizationalUnitType() {
+	public OrganizationalUnitType getOrganizationalUnitType() {
 		return organizationalUnitType;
 	}
-	public void setOrganizationalUnitType(String organizationalUnitType) {
+	public void setOrganizationalUnitType(OrganizationalUnitType organizationalUnitType) {
 		this.organizationalUnitType = organizationalUnitType;
+	}
+	
+	public enum OrganizationalUnitType {
+		DEPARTMENT,
+		DIVISION,
+		PROGRAM
 	}
 	
 	
