206f3dd488adabba09bd4d766cecbbc2ab0d251e
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-12-17 13:30:50 -0500

    add publication type main class

---
+++ src/main/java/reciter/engine/analysis/ReCiterArticlePublicationType.java
@@ -0,0 +1,24 @@
+package reciter.engine.analysis;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import reciter.model.article.ReCiterPublicationTypeScopus;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@DynamoDBDocument
+public class ReCiterArticlePublicationType {
+	
+	private String publicationTypeCanonical;
+	private String publicationTypePubMed;
+	private ReCiterPublicationTypeScopus publicationTypeScopus;
+}
