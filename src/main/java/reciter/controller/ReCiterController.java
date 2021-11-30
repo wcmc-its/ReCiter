3fb0e7ce8206590576e7fd2f7a4d588b296f5d8c
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-11-30 14:02:12 -0500

    add group of uids for group api as filter

--- src/main/java/reciter/controller/ReCiterController.java
+++ src/main/java/reciter/controller/ReCiterController.java
@@ -345,21 +345,23 @@ public class ReCiterController {
     })
     @RequestMapping(value = "/reciter/feature-generator/by/group", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public ResponseEntity retrieveBulkFeatureGenerator(@RequestParam(required =false) List<String> personType, @RequestParam(required = false) List<String> organizationalAffiliation, @RequestParam(required = false) List<String> departmentalAffiliation,
+    public ResponseEntity retrieveBulkFeatureGenerator(@RequestParam(required =false) List<String> uids, @RequestParam(required =false) List<String> personType, @RequestParam(required = false) List<String> organizationalAffiliation, @RequestParam(required = false) List<String> departmentalAffiliation,
     		@RequestParam(required = true) Double totalStandardizedArticleScore, @RequestParam(required = true) int maxArticlesPerPerson) {
         StopWatch stopWatch = new StopWatch("Retrieve pending articles for a group of users");
         stopWatch.start("Retrieve pending articles for a group of users");
-        
-        List<Identity> identities;
-        try {
-            identities = identityService.findAll();
-        } catch (Exception ne) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Issue with the request" + ne);
-        }
-        if(identities.size() == 0) {
-            stopWatch.stop();
-            log.info(stopWatch.getId() + " took " + stopWatch.getTotalTimeSeconds() + "s");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Identity table is empty.");
+        List<Identity> identities = null;
+        if(uids == null || uids.isEmpty()) {
+            
+            try {
+                identities = identityService.findAll();
+            } catch (Exception ne) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Issue with the request" + ne);
+            }
+            if(identities.size() == 0) {
+                stopWatch.stop();
+                log.info(stopWatch.getId() + " took " + stopWatch.getTotalTimeSeconds() + "s");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Identity table is empty.");
+            }
         }
         
         final double totalScore;
@@ -370,34 +372,42 @@ public class ReCiterController {
         	totalScore = totalStandardizedArticleScore;
         }
         
-        if(identities != null && identities.size() > 0) {
-        	List<String> identitySubset = identities.parallelStream().filter(identity -> 
-        			((personType == null)?
-        			true:
-        			(identity.getPersonTypes() != null
-        			&&
-        			!identity.getPersonTypes().isEmpty() && !Collections.disjoint(identity.getPersonTypes(), personType)))
-        			&&
-        			((organizationalAffiliation == null)?
-        			true:
-        			(identity.getInstitutions() != null
-        			&&
-        			!identity.getInstitutions().isEmpty()
-        			&&
-        			!Collections.disjoint(identity.getInstitutions(), organizationalAffiliation)))
-        			&&
-        			((departmentalAffiliation == null)?
-        			true:		
-        			(identity.getOrganizationalUnits() != null
-        			&&
-        			!identity.getOrganizationalUnits().isEmpty()
-        			&&
-        			!Collections.disjoint(identity.getOrganizationalUnits().stream()
-        					.map(OrganizationalUnit::getOrganizationalUnitLabel)
-        					.collect(Collectors.toList()), departmentalAffiliation))))
-        			.map(Identity::getUid)
-        			.collect(Collectors.toList());
-        	List<AnalysisOutput> analysis = analysisService.findByUids(identitySubset);
+        if((identities != null && !identities.isEmpty()) || (uids != null && !uids.isEmpty())) {
+            List<String> identitySubset = null;
+            if(identities != null && !identities.isEmpty()) {
+                identitySubset = identities.parallelStream().filter(identity -> 
+                        ((personType == null)?
+                        true:
+                        (identity.getPersonTypes() != null
+                        &&
+                        !identity.getPersonTypes().isEmpty() && !Collections.disjoint(identity.getPersonTypes(), personType)))
+                        &&
+                        ((organizationalAffiliation == null)?
+                        true:
+                        (identity.getInstitutions() != null
+                        &&
+                        !identity.getInstitutions().isEmpty()
+                        &&
+                        !Collections.disjoint(identity.getInstitutions(), organizationalAffiliation)))
+                        &&
+                        ((departmentalAffiliation == null)?
+                        true:		
+                        (identity.getOrganizationalUnits() != null
+                        &&
+                        !identity.getOrganizationalUnits().isEmpty()
+                        &&
+                        !Collections.disjoint(identity.getOrganizationalUnits().stream()
+                                .map(OrganizationalUnit::getOrganizationalUnitLabel)
+                                .collect(Collectors.toList()), departmentalAffiliation))))
+                        .map(Identity::getUid)
+                        .collect(Collectors.toList());
+            }
+            List<AnalysisOutput> analysis = null;
+            if(uids != null && !uids.isEmpty()) {
+                analysis = analysisService.findByUids(uids);
+            }  else if(identitySubset != null && !identitySubset.isEmpty()){
+                analysis = analysisService.findByUids(identitySubset);
+            }
         	if (analysis != null && !analysis.isEmpty()) {
         		analysis.stream().forEach(anl -> {
         			if(anl.getReCiterFeature() != null
