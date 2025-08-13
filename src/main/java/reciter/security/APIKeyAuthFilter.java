b69bc37abab0c60b430876dfe454cebfc075a11b
mrj4001 <mrj4001@med.cornell.edu>
2025-08-13 10:57:31 +0530

    implemented the Spring Security for JWT Token and ApiKey

--- src/main/java/reciter/security/APIKeyAuthFilter.java
+++ src/main/java/reciter/security/APIKeyAuthFilter.java
@@ -7,14 +7,30 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
 public class APIKeyAuthFilter  extends AbstractPreAuthenticatedProcessingFilter {
 	
 	private String principalRequestHeader;
+	private String authorizationHeader;
 
-    public APIKeyAuthFilter(String principalRequestHeader) {
+    public APIKeyAuthFilter(String principalRequestHeader, String authorizationHeader) {
         this.principalRequestHeader = principalRequestHeader;
+        this.authorizationHeader = authorizationHeader;
     }
 
 	@Override
 	protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-		return request.getHeader(principalRequestHeader);
+		//return request.getHeader(principalRequestHeader);
+		String authHeader = request.getHeader(authorizationHeader);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            // You can choose to return the token or a decoded user ID (e.g., from gateway)
+            return token;// Or extract user info if needed
+        }
+
+        // Fallback to API Key
+        String apiKey = request.getHeader(principalRequestHeader);
+        if (apiKey != null) {
+            return apiKey;
+        }
+
+        return null;
 	}
 
 	@Override
