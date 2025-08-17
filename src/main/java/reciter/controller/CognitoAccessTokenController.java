3344787aedb773233b8ea0a120dfc3518754df8d
mrj4001 <mrj4001@med.cornell.edu>
2025-08-17 21:33:46 +0530

    Added crossorigins to accept the request from the specific url to this API

--- src/main/java/reciter/controller/CognitoAccessTokenController.java
+++ src/main/java/reciter/controller/CognitoAccessTokenController.java
@@ -3,18 +3,15 @@ package reciter.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import reciter.security.AwsSecretsManagerService;
 import reciter.service.aws.CognitoTokenService;
 
@@ -30,7 +27,8 @@ public class CognitoAccessTokenController {
 	@Autowired
     private CognitoTokenService cognitoTokenService; 
 	
-	
+	@CrossOrigin(origins = "https://reciter-consumer.weill.cornell.edu",
+				 methods = {RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping("/generate-access-token")
     public ResponseEntity<String> generateAccessToken(@RequestParam(value = "clientName") String clientName) {
        
