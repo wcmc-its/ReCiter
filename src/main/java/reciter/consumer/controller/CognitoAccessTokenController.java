cb223f116ba85f3b791ca7f5f8ea7e6ba441397d
Veerraju <ved4006@med.cornell.edu>
2025-09-18 18:42:32 +0530

    removed duplicated class and removed sysout statement

--- src/main/java/reciter/consumer/controller/CognitoAccessTokenController.java
+++ src/main/java/reciter/consumer/controller/CognitoAccessTokenController.java
@@ -1,23 +1,15 @@
 package reciter.consumer.controller;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import reciter.consumer.dto.TokenInfo;
-import reciter.consumer.exception.ApiKeyMappingNotFoundException;
 import reciter.consumer.response.ApiResponse;
 import reciter.consumer.service.CognitoTokenService;
 import reciter.security.AwsSecretsManagerService;
