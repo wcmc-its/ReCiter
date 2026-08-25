703c452b909a3a4b3dfab22f07afe85812b29fe9
“ved4006” <ved4006@med.cornell.edu>
2026-08-25 19:01:06 +0530

    remove un-used import statements

--- src/main/java/reciter/controller/ExternalArticleController.java
+++ src/main/java/reciter/controller/ExternalArticleController.java
@@ -16,10 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
